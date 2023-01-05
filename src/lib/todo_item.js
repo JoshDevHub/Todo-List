35e2481d1cca39a2ceb17e32a5c7660bc929f86c
jmsmith1018 <jmsmith1018@gmail.com>
2023-01-05 12:38:58 -0700

    feat: add class for `TodoItem`

---
+++ src/lib/todo_item.js
@@ -0,0 +1,9 @@
+// class for a single item in a todo list
+class TodoItem {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
