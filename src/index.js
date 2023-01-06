9f6cb541a479c01514e2eca3a1bd2cc88d64bc31
jmsmith1018 <jmsmith1018@gmail.com>
2023-01-06 13:33:32 -0700

    feat: link up styles and button and modal components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 // entry point script
 
+import "./styles/reset.css";
+import "./styles/app.css";
+
 import renderHeader from "./components/header";
+import Modal from "./components/modal";
+import Button from "./components/button";
+
+const modal = new Modal();
 
 renderHeader();
+modal.render();
 
+const addTodoHandler = () => {
+  modal.toggle();
+}
+
+new Button("Add Todo", addTodoHandler).render();
