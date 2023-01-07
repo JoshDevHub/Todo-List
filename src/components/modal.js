19d00db1fd6f5235f388802f3a50b45269bda1e2
jmsmith1018 <jmsmith1018@gmail.com>
2023-01-06 18:58:51 -0700

    feat: reorganize `Modal` into a singleton

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,25 +1,21 @@
 import { buildElement } from "./dom_helpers";
-import Button from "./button";
 
-export default class Modal {
+class Modal {
   constructor() {
     this.modalOverlay = buildElement({ attributes: { class: "modal-overlay closed" } });
     this.modalBox = buildElement({ attributes: { class: "modal-box closed" } });
   }
 
   toggle() {
-    const overlay = this.modalOverlay ?? document.querySelector(".modal-overlay");
-    const box = this.modalBox ?? document.querySelector(".modal-box");
-    overlay.classList.toggle("closed");
-    box.classList.toggle("closed");
+    this.modalOverlay.classList.toggle("closed");
+    this.modalBox.classList.toggle("closed");
   }
 
   render() {
-    if (document.querySelector(".modal-overlay")) return;
-
-    new Button("Cancel", this.toggle).render(this.modalBox);
-
     document.body.appendChild(this.modalOverlay);
     document.body.appendChild(this.modalBox);
   }
 }
+
+const modal = new Modal();
+export default modal;
