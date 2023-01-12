0144921132cf4eab778b949d1380b2a73387db6c
jmsmith1018 <jmsmith1018@gmail.com>
2023-01-12 12:17:59 -0700

    feat: add form component for creating new projects

---
+++ src/components/project_form.js
@@ -0,0 +1,54 @@
+import { buildElement } from "../utils/dom_helpers";
+import modal from "./modal";
+import Project from "../lib/project";
+
+const createProjectForm = (projectManager) => {
+  const entryPoint = document.querySelector(".modal-box");
+
+  const projectFromInputs = () => {
+    const projectName = document.getElementById("name").value;
+    return new Project(projectName);
+  }
+
+  const buildForm = () => {
+    return buildElement({
+      tag: "form",
+      children: [
+        { tag: "label", text: "name", attributes: { for: "name" } },
+        { tag: "input", attributes: { type: "text", id: "name", name: "name" } },
+      ]
+    })
+  }
+
+  const addProjectHandler = (event) => {
+    event.preventDefault();
+    const newProject = projectFromInputs();
+    projectManager.addProject(newProject);
+    projectManager.setCurrentProject(newProject);
+    modal.toggle();
+  }
+
+  const render = () => {
+    entryPoint.replaceChildren();
+
+    const form = buildForm();
+
+    const button = buildElement({
+      tag: "button",
+      text: "Add Project",
+      attributes: { type: "button" }
+    })
+    button.addEventListener("click", addProjectHandler);
+    form.appendChild(button);
+
+    const fragment = document.createDocumentFragment();
+    fragment.appendChild(form);
+    entryPoint.appendChild(fragment);
+  }
+
+  return { render };
+}
+
+export default function renderProjectForm(projectManager) {
+  createProjectForm(projectManager).render();
+}
