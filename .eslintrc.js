15f75996cb36ab3fe81448e9f7b21cab77ed95ab
Josh Smith <jmsmith1018@gmail.com>
2023-02-14 22:54:47 -0700

    chore: add eslint to project

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "prettier"
+  ],
+  "rules": {
+    "eqeqeq": "warn"
+  },
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  }
+}
